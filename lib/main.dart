2c7af8b880755cd3a21ec93a32492896c435f66d
tariq_anwer <tariqanwer2009@gmail.com>
2024-08-29 15:26:46 +0000

    work Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,178 @@
-import 'package:flutter/material.dart';
+ //question 1
+   import 'dart:io';
 
-void main() => runApp(MyApp());
+void main() {
+  print("Enter a number: ");
+  int num = int.parse(stdin.readLineSync()!);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  if (num > 0) {
+    print("$num is a positive number.");
+  } else if (num < 0) {
+    print("$num is a negative number.");
+  } else {
+    print("$num is zero.");
+  }
+} 
+//question 2
+import 'dart:io';
+
+void main() {
+  print("Enter a number: ");
+  int number = int.parse(stdin.readLineSync()!);
+
+  if (number % 2 == 0) {
+    print("$number is even");
+  } else {
+    print("$number is odd");
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+//Question 2
+  import 'dart:io';
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
+void main() {
+  print("Enter a year: ");
+  int year = int.parse(stdin.readLineSync()!);
+
+  if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+    print("$year is a leap year");
+  } else {
+    print("$year is not a leap year");
   }
+}
+
+//question 4
+ import 'dart:io';
+
+void main() {
+  print("Enter the first number: ");
+  double num1 = double.parse(stdin.readLineSync()!);
+
+  print("Enter the second number: ");
+  double num2 = double.parse(stdin.readLineSync()!);
+
+  print("Enter the third number: ");
+  double num3 = double.parse(stdin.readLineSync()!);
+
+  if (num1 >= num2 && num1 >= num3) {
+    print("$num1 is the largest number");
+  } else if (num2 >= num1 && num2 >= num3) {
+    print("$num2 is the largest number");
+  } else {
+    print("$num3 is the largest number");
+  }
+}
+//question 5
+  import 'dart:io';
+
+void main() {
+  print("Enter a year: ");
+  int year = int.parse(stdin.readLineSync()!);
+
+  if (year % 100 == 0) {
+    print("$year is a century year");
+  } else {
+    print("$year is not a century year");
+  }
+}
+  //question 6
+  import 'dart:io';
+  void main() {
+  print("number is divisible by 5 and 11: ");
+  int number = int.parse(stdin.readLineSync()!);
+
+  if (number % 5 == 0 && number % 11 == 0) {
+    print("$number is divisible by 5 and 11");
+  } else {
+    print("$number is not divisible by 5 and 11  ");
+  }
+}
+//question 7
+import 'dart:io';
+  void main() {
+  print("number is multiple by 3 and 7: ");
+  int number = int.parse(stdin.readLineSync()!);
+
+  if (num % 3 == 0 && num % 7 == 0) {
+    print("$number is multiple by 3 and 7");
+  } else {
+    print("$number is not multiple by 3 and 7  ");
+  }
+}
+//question 8
+void main() {
+  print("Enter marks for Subject 1: ");
+  int subject1 = int.parse(stdin.readLineSync()!);
+
+  print("Enter marks for Subject 2: ");
+  int subject2 = int.parse(stdin.readLineSync()!);
+
+  print("Enter marks for Subject 3: ");
+  int subject3 = int.parse(stdin.readLineSync()!);
+
+  print("Enter marks for Subject 4: ");
+  int subject4 = int.parse(stdin.readLineSync()!);
+
+  int totalMarks = subject1 + subject2 + subject3 + subject4;
+  double percentage = (totalMarks / 400) * 100;
+
+  print("Marksheet\n");
+  print("Subject 1: $subject1\n");
+  print("Subject 2: $subject2\n");
+  print("Subject 3: $subject3\n");
+  print("Subject 4: $subject4\n");
+  print("Total Marks: $totalMarks/400\n");
+  print("Percentage: $percentage%\n");
+}
+
+//question 8
+void main() {
+  print("Enter marks for Subject 1: ");
+  int subject1 = int.parse(stdin.readLineSync()!);
+
+  print("Enter marks for Subject 2: ");
+  int subject2 = int.parse(stdin.readLineSync()!);
+
+  print("Enter marks for Subject 3: ");
+  int subject3 = int.parse(stdin.readLineSync()!);
+
+  print("Enter marks for Subject 4: ");
+  int subject4 = int.parse(stdin.readLineSync()!);
+
+  int totalMarks = subject1 + subject2 + subject3 + subject4;
+  double percentage = (totalMarks / 400) * 100;
+
+  print("Marksheet\n");
+  print("Subject 1: $subject1\n");
+  print("Subject 2: $subject2\n");
+  print("Subject 3: $subject3\n");
+  print("Subject 4: $subject4\n");
+  print("Total Marks: $totalMarks/400\n");
+  print("Percentage: $percentage%\n");
+}
+
+//question 8
+void main() {
+  print("Enter marks for Subject 1: ");
+  int subject1 = int.parse(stdin.readLineSync()!);
+
+  print("Enter marks for Subject 2: ");
+  int subject2 = int.parse(stdin.readLineSync()!);
+
+  print("Enter marks for Subject 3: ");
+  int subject3 = int.parse(stdin.readLineSync()!);
+
+  print("Enter marks for Subject 4: ");
+  int subject4 = int.parse(stdin.readLineSync()!);
+
+  int totalMarks = subject1 + subject2 + subject3 + subject4;
+  double percentage = (totalMarks / 400) * 100;
+
+  print("Marksheet\n");
+  print("Subject 1: $subject1\n");
+  print("Subject 2: $subject2\n");
+  print("Subject 3: $subject3\n");
+  print("Subject 4: $subject4\n");
+  print("Total Marks: $totalMarks/400\n");
+  print("Percentage: $percentage%\n");
 }
